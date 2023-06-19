367d1798b5aab8a4884e327e56c36670965a9270
Dark-WizZ <mailwithakram@gmail.com>
2023-06-19 15:59:38 +0530

    Fix dropdown toggle for chrome support

--- src/comps/Item.jsx
+++ src/comps/Item.jsx
@@ -6,10 +6,10 @@ function Item({obj}) {
   const [showDetails, setShowDetails] = useState(false)
   const dropDown= (e)=>{
     if(showDetails){
-      e.nativeEvent.originalTarget.classList.toggle('rotate')
+      e.target.classList.toggle('rotate')
       setShowDetails(false)
     }else{
-      e.nativeEvent.originalTarget.classList.toggle('rotate')
+      e.target.classList.toggle('rotate')
       setShowDetails(true)
     }
   }
